03a6d6810f71897207d3c05e2d70c2b0f5d1c196
Your Mom <skibski.eugene@gmail.com>
2023-01-04 19:39:42 +0300

    my first csharp project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+int a = 0, b = 0;
+a = int.Parse(Console.ReadLine());
+b = int.Parse(Console.ReadLine());
+if(a > b){
+    Console.WriteLine(a);
+}else{
+    Console.WriteLine(b);
+}
